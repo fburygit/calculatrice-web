b9681af8c4e8cd62d12b15c515272b7c45b13181
farouck98 <latoundjifarouck@gmail.com>
2025-10-17 15:49:33 +0200

    new files added

--- script.js
+++ script.js
@@ -2,12 +2,35 @@ document.addEventListener("DOMContentLoaded", () => {
   const display = document.getElementById("display");
   const buttons = document.getElementById("buttons");
 
-  const symbols = ["7", "8", "9", "+", "4", "5", "6", "-", "1", "2", "3", "*", "0", "C", "=", "/"];
-  symbols.forEach(sym => {
+  const symbols = [
+    "7",
+    "8",
+    "9",
+    "+",
+    "4",
+    "5",
+    "6",
+    "-",
+    "1",
+    "2",
+    "3",
+    "*",
+    "0",
+    "C",
+    "=",
+    "/",
+  ];
+  symbols.forEach((sym) => {
     const btn = document.createElement("button");
     btn.textContent = sym;
     buttons.appendChild(btn);
   });
 
   console.log("Calculatrice initialisée !");
+});
+
+import { div } from "../script.js";
+
+test("division simple", () => {
+  expect(div(6, 3)).toBe(2);
 });
